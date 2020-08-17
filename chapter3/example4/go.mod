cedf70e14ceaaaf62d789956f776e3a30168c5d2
Baheer-Kamal <baheer.kamal@ibm.com>
2020-08-16 21:05:51 -0400

    added example 4 for chapter 3

---
+++ chapter3/example4/go.mod
@@ -0,0 +1,8 @@
+module example4
+
+go 1.14
+
+require (
+	github.com/Tanmay-Teaches/golang/chapter3/example3 v0.0.0-20200817005752-e03d213ecc87
+	github.com/labstack/echo/v4 v4.1.16
+)
--- chapter3/example4/main.go
+++ chapter3/example4/main.go
@@ -1,2 +1,35 @@
 package main
 
+/*
+Create a simple API endpoint which return true if number is prime
+
+To use private repository or private git like github enterprise use ssh keys over username/password.
+	The following command change all URL from https to git which use ssh keys
+
+		git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+	Golang will also try to do a sum check, this will also need to be disable for private repo
+		export GONOSUMDB=github.com/Tanmay-Teaches/golang
+*/
+import (
+	"net/http"
+	"github.com/Tanmay-Teaches/golang/chapter3/example3"
+	"github.com/labstack/echo/v4"
+	"strconv"
+)
+
+
+func main() {
+
+	e := echo.New()
+	e.GET("/:number", func(c echo.Context) error {
+		nstr := c.Param("number")
+		n, err := strconv.Atoi(nstr)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+		return c.String(http.StatusOK, strconv.FormatBool(example3.IsPrime(n)))
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
