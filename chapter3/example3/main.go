674444f8cfb8e9f755f9cbd1c3e7836f45c6e27a
Baheer-Kamal <baheer.kamal@ibm.com>
2020-08-16 20:32:43 -0400

    add example 3

---
+++ chapter3/example3/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+Example of how to create an Package in Golang
+*/
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n <= 3 {
+		return true
+	} else if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	i := 5
+	for i*i < n {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+		i += 6
+	}
+	return true
+}
+
+func main() {
+	for i := 0; i < 100; i++ {
+		println(i, ":", isPrime(i))
+	}
+}
